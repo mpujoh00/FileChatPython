18573dbc2f94cba1722ba19c9dbbfd37e6ba7af3
Micaela Pujol <mpujolh>
2021-09-12 15:13:49 +0200

    final

---
+++ Client.py
@@ -0,0 +1,194 @@
+import socket
+import sys
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import os.path
+from pathlib import Path
+import errno
+
+JAVA_HOST = '192.168.1.37'
+JAVA_PORT = 2021
+PYTHON_HOST = '192.168.56.101'
+PYTHON_PORT = 2021
+
+
+def send_message(message):
+    try:
+        sock.send(bytes(message + "\r\n", "UTF-8"))
+    except socket.error:
+        pass
+
+
+def read_message():
+    message = ""
+    try:
+        message = sock.recv(1024).decode(encoding="UTF-8").rstrip()
+    except socket.error:
+        raise
+    if message == "":
+        raise socket.error
+    return message
+
+
+def read_file_python(f, fSize):
+    readBytes = 0
+    while fSize != readBytes:
+        # reception confirmation message
+        send_message("ok")
+        # reads bytes
+        data = sock.recv(8192)
+        # writes to file
+        f.write(data)
+        # updates amount of read bytes
+        readBytes += len(data)
+    f.close()
+
+def read_file_java(f, fSize):
+    # sends confirmation message
+    send_message("ok")
+    while fSize > 0:
+        data = sock.recv(1024)
+        file.write(data)
+        fSize -= len(data)
+    f.close()
+
+def send_file_java(f):
+    # gets file data
+    data = f.read(8192)
+    while data:
+        # waits for reception confirmation message
+        read_message()
+        # sends bytes
+        sock.send(data)
+        data = f.read(8192)
+    # waits for confirmation message
+    read_message()
+
+def connect_to_server(host, port):
+    print("Python client connecting to server of ip: " + str(host) + " port: " + str(port))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        return s
+    except socket.error:
+        print("Couldn't connect to the server")
+        raise
+
+# connects with the server
+try:
+    sock = connect_to_server(JAVA_HOST, JAVA_PORT)
+except socket.error:
+    try:
+        sock = connect_to_server(PYTHON_HOST, PYTHON_PORT)
+    except socket.error:
+        sys.exit()
+
+# gets what server it is
+server = read_message()
+print("Connected to " + server + " server")
+# indicates what client it is
+send_message("python")
+
+print("Waiting for messages")
+run = True
+# reads messages from the server
+while run:
+    try:
+        receivedMessage = read_message()
+        if receivedMessage == "What file do you want to send?":
+            # sends file to a friend
+            print(" - " + receivedMessage)
+            # chooses file to sendprint("")
+            Tk().withdraw()
+            desktop = os.path.expanduser("~/Desktop")
+            filepath = askopenfilename(initialdir=desktop)
+            # checks if user canceled operation
+            if not filepath:
+                send_message("q")
+            # file chosen
+            else:
+                # sends file size
+                file = open(filepath, "rb")
+                fileSize = Path(filepath).stat().st_size
+                send_message(str(fileSize))
+                # waits to know if the file size is allowed
+                validSize = read_message()
+                # file size allowed
+                if validSize == "ok":
+                    # sends filename
+                    filepath = filepath.split("/")
+                    filename = filepath[len(filepath) - 1]
+                    send_message(filename)
+                    # waits to know if the file extension is allowed
+                    validExtension = read_message()
+                    if validExtension == "ok":
+                        # gets file data
+                        line = file.read(8192)
+                        while line:
+                            # waits for reception confirmation message
+                            read_message()
+                            # sends bytes
+                            sock.send(line)
+                            line = file.read(8192)
+                        file.close()
+                        print("- File received by user")
+                    else:
+                        print(" - " + validExtension)
+                else:
+                    print(" - " + validSize)
+
+        elif receivedMessage.startswith("You have received a new file from"):
+            print(" - " + receivedMessage)
+            # gets file name
+            filename = "received files/" + receivedMessage.split(": ")[1]
+            # creates path if it didn't exist
+            if not os.path.exists(os.path.dirname(filename)):
+                try:
+                    os.makedirs(os.path.dirname(filename))
+                except OSError as exc:
+                    if exc.errno != errno.EEXIST:
+                        raise
+            # creates file
+            file = open(filename, "wb")
+            # gets file size
+            size = int(sock.recv(1024))
+            # writes file
+            if server == "python":
+                read_file_python(file, size)
+            else:
+                read_file_java(file, size)
+            # sends confirmation message
+            send_message("ok")
+
+        elif receivedMessage == "Goodbye!":
+            print(" - " + receivedMessage)
+            run = False
+        elif receivedMessage == "":
+            print("hello")
+            print("")
+        else:
+            print(" - " + receivedMessage)
+
+        # expects an answer
+        if receivedMessage.endswith(":"):
+            send_message(input())
+
+    except socket.error:
+        # tries to connect to other server
+        print("Server failed, trying with another")
+        try:
+            if server == "java":
+                print("Connecting to Python server")
+                sock = connect_to_server(PYTHON_HOST, PYTHON_PORT)
+            else:
+                print("Connecting to Java server")
+                sock = connect_to_server(JAVA_HOST, JAVA_PORT)
+            server = read_message()
+            print("Connected to " + server + " server")
+            send_message("python")
+        except socket.error:
+            # ends program
+            print("There isn't any available server")
+            run = False
+
+sock.close()
---
+++ Server.py
@@ -0,0 +1,56 @@
+from ClientHandler import ClientHandler
+
+from Database import Database
+import socket
+import threading
+import time
+
+global stopDatabase
+
+class Server:
+    def __init__(self):
+        # database
+        try:
+            self.database = Database()
+        except Exception:
+            # database connection fail, can't run without database
+            raise
+
+        # keeps it alive
+        global stopDatabase
+        stopDatabase = False
+        self.databaseThread = threading.Thread(target=self.keep_database_alive)
+        self.databaseThread.start()
+        # creates server socket
+        self.HOST = '192.168.56.101'
+        self.PORT = 2021
+        self.socketServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start_server(self):
+        # creation of the socket
+        self.socketServer.bind((self.HOST, self.PORT))
+        # listening for clients
+        self.socketServer.listen(5)
+        print("Python server listening on ip: " + self.HOST + " port: " + str(self.PORT))
+
+        while True:
+            print("Waiting for a client connection request")
+            # accepts connection with a client and creates a thread for it
+            ClientHandler(self.socketServer.accept(), self.database).start()
+
+    def keep_database_alive(self):
+        global stopDatabase
+        while not stopDatabase:
+            time.sleep(5)
+            self.database.keep_alive()
+
+    def stop_server(self):
+        global stopDatabase
+        stopDatabase = True
+        self.socketServer.close()
+
+try:
+    server = Server()
+    server.start_server()
+except Exception as e:
+    print("Couldn't connect to the database", e)
